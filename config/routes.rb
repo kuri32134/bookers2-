fa9bb8ee6a322c330b37a0c956b6404d55a7eda7
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-04-27 08:28:10 +0000

    [add]header footer aboutページ sign_inの編集 名前でログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  
   devise_for :users
   root to: "homes#top"
   get 'homes/about'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
