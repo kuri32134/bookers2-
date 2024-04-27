8762466c3f8dfca68815d42941fb259c360b8a1d
kuri32134 <161860576+kuri32134@users.noreply.github.com>
2024-04-27 14:19:24 +0000

    [add]user_edit user_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: "homes#top"
   get 'homes/about'
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
